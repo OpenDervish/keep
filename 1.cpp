e2c8c5811cf9aca5ed5646ef892223e5307dfa11
OpenDervish <89380993+OpenDervish@users.noreply.github.com>
2023-07-07 01:48:59 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -18,6 +18,8 @@ int mt=1;
 int flg=1;
 int r,s;
 int sel=0;
+int alt=1;
+
 GLfloat vertices [][3] = {{1150.0,550.0,-50.0}, {1250.0,550.0,-50.0}, 
                           {1250.0,650.0,-50.0}, {1150.0,650.0,-50.0},
                           {1150.0,550.0,50.0}, {1250.0,550.0,50.0},
@@ -45,18 +47,19 @@ void drawStringAtPos(double x, double y,double z,string msg)
 		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, msg[i]);
 }
 
-void reshape(int w,int h)
+void reshape(int w,int h)	
 {
-        width=w;height=h;
-    	glViewport(0,0,w,h);
-    	glMatrixMode(GL_PROJECTION);
-    	glLoadIdentity();
-        glOrtho(0.0,1367.0,0.0,767.0, -180.0,180.0);
-        glMatrixMode(GL_MODELVIEW);
-        glClearColor(1.0,1.0,0.8,1.0);
+	width=w;
+	height=h;
+	glViewport(0,0,w,h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0,1367.0,0.0,767.0, -180.0,180.0);
+	glMatrixMode(GL_MODELVIEW);
+	glClearColor(1.0,1.0,0.8,1.0);
 }
 
-void polygon(int a,int b,int c,int d,int e)
+void polygon(int a,int b,int c,int d,int e)			
 {
 	glColor3f(0,0,1);
 	glBegin(GL_POLYGON);			//dice face
@@ -81,11 +84,9 @@ void polygon(int a,int b,int c,int d,int e)
     	glBegin(GL_POINTS);
         switch(e)		//number on dice
         {
-            case 1 :
-                        glVertex3f(1200.0,600.0,-50.0);
-                        break;
-            case 2 :
-                        glVertex3f(1175.0,651.0,25.0);
+            case 1 :glVertex3f(1200.0,600.0,-50.0);
+					break;
+            case 2 :    glVertex3f(1175.0,651.0,25.0);
                         glVertex3f(1225.0,651.0,-25.0);
                         break;
             case 3 :    glVertex3f(1149.0,575.0,25.0);
@@ -127,14 +128,14 @@ void colorcube()
 void rect()         //board
 {
 	int i,j;
-	x[-1]=x0;
-	y[-1]=y2;
+	x[-1]=x0;			//define left boundary
+	y[-1]=y2;			//define top boundary
 	for( i=0;i<=maxx;i++)
 		x[i]=x0+i*dx;
 	for( j=0;j<=maxy;j++)
 		y[j]=y2+j*dy;
 	glColor3f(0.88,0.88,0.88);
-	glBegin(GL_QUADS);			//outer-boundaries of board
+	glBegin(GL_QUADS);			//outer-boundaries of board->square
         	glVertex3f(x0,y2,10);
         	glVertex3f(x[maxx],y2,10);
         	glVertex3f(x[maxx],y[maxy],10);
@@ -145,7 +146,7 @@ void rect()         //board
 	i=0;
 	for(j=0;j<maxy;j++)
 	{
-		while(1)
+		while(1)			//each horizontal row
 		{
 			if(temp<0)
 			{
@@ -181,7 +182,7 @@ void rect()         //board
 			count++;
 			i+=temp;
 		}
-		if(temp>0)
+		if(temp>0)			//zig-zag board path
 		{
 			temp=-1;
 		}
@@ -198,7 +199,7 @@ void snake_head(int xr,int yr)
 	int i;
 	glColor3f(0.50,0.0,0.502);
 	glBegin(GL_LINE_LOOP);
-    for(i=0;i<360;i++)
+    for(i=0;i<360;i++)		
     {
         float rad = i*DEG2RAD;
         glVertex2f(cos(rad)*xr,sin(rad)*yr);
@@ -213,7 +214,7 @@ void snake_head(int xr,int yr)
 	}
 
 	glEnd();
-    glScaled(1,2,0.5);
+	glScaled(1,2,0.5);
 	glFlush();
 }
 
@@ -254,36 +255,36 @@ void snakes()
 			lc++;
 		}
 		glPushMatrix();
-        		glTranslatef(xsnake[j],zsnake[j],18);
-        		glColor3f(255/255.0, 51/255.0, 0);
-        		glPointSize(3.0);
-       			glBegin(GL_POINTS);			//eyes of snake
-        			glVertex3f(-12.5,7.5,20);
-        			glVertex3f(12.5,7.5,20);
-       	 		glEnd();
-        		glBegin(GL_POLYGON);			//teeth of snake
-            			glVertex3f(0,-1,20);
-            			glVertex3f(-4,-13,20);
-            			glVertex3f(-4,-1,20);
-            			glVertex3f(4,-1,20);
-            			glVertex3f(4,-13,20);
-       			glEnd();
-        		snake_head(25,15);
-        	glPopMatrix();
+			glTranslatef(xsnake[j],zsnake[j],18);
+			glColor3f(255/255.0, 51/255.0, 0);
+			glPointSize(3.0);
+			glBegin(GL_POINTS);			//eyes of snake
+				glVertex3f(-12.5,7.5,20);
+				glVertex3f(12.5,7.5,20);
+			glEnd();
+			glBegin(GL_POLYGON);			//teeth of snake
+					glVertex3f(0,-1,20);
+					glVertex3f(-4,-13,20);
+					glVertex3f(-4,-1,20);
+					glVertex3f(4,-1,20);
+					glVertex3f(4,-13,20);
+			glEnd();
+			snake_head(25,15);
+        glPopMatrix();
 	}
 }
 pawns::pawns(GLfloat p1,GLfloat q1,GLfloat col,GLint tmp)
 {
 	add=speed,up=0,flag=1;
-	this->p1=p1;
+	this->p1=p1;			//pawn dimensions x
 	this->p2=p1-10;
 	this->p3=p1;
 	this->p4=p1+10;
-	this->q1=q1;
+	this->q1=q1;			//pawn dimensions y
 	this->q2=q1+10;
 	this->q3=q1+20;
 	this->q4=q1+10;
-	this->col1=col;
+	this->col1=col;			//colour
 	oy=q1-y2;
 	this->tmp=tmp;			//stop speed
 }
@@ -303,6 +304,104 @@ void pawns::disp()
 void pawns::init(int r)
 {
 	this->t=r;
+}
+
+void IR3(int i)
+{
+	if(alt==0)
+		paw1.tryy(i);
+	else
+		paw2.tryy(i);
+}
+float snlf(float y,float m,float c)
+{
+	return (y-c)/m;
+}
+void IR4(int i)
+{
+	if(alt==0)
+		paw1.try2(i);
+	else
+		paw2.try2(i);
+}
+void pawns::tryy(int i)			//ladders
+{
+	mt=0;
+	if((l[i].yq*70.0+30+oy)>q1)
+    {
+        q1+=speed;
+        q2+=speed;
+        q3+=speed;
+        q4+=speed;
+        p1=snlf(q1,m,c);
+        p2=p1-10;
+        p3=p1;
+        p4=p1+10;
+        glutPostRedisplay();
+        glutTimerFunc(1,IR3,i);			//IR3 delay
+	}
+	else
+		mt=1;
+}
+void pawns::try2(int i)			//snakes
+{
+	mt=0;
+	if((sn[i].yq*70+30+oy)<q1)
+	{
+		q1-=speed;
+		q2-=speed;
+		q3-=speed;
+		q4-=speed;
+		if(m!=0)
+		{
+            p1=snlf(q1,m,c);
+            p2=p1-10;
+            p3=p1;
+            p4=p1+10;
+		}
+		glutPostRedisplay();
+		glutTimerFunc(1,IR4,i);			//IR4 delay
+	}
+		else mt=1;
+}
+void pawns::check()
+{
+	int a=(p1-350)/70;
+	int b=(q1-30)/70;
+	int i;
+	for(i=0;i<4;i++)
+	{
+		if(a==l[i].xp&&b==l[i].yp)			//check ladder matching
+		{
+
+			m=(l[i].yp*70.0+y2+oy-(l[i].yq*70.0+y2+oy))/(l[i].xp*70.0+x0+30-(l[i].xq*70.0+x0+30));			//move up that ladder
+			c=(l[i].yp*70.0+y2+oy-(m*(l[i].xp*70.0+x0+30)));
+			this->tryy(i);
+			if(((l[i].yq-l[i].yp)%2))
+			{
+				flag=!flag;
+				add*=(-1);
+			}
+		}
+	}
+	for(i=0;i<4;i++)
+	{
+		if(a==sn[i].xp&&b==sn[i].yp)			//check snakes matching
+		{
+			if(sn[i].xp==sn[i].xq)
+				m=0;
+			else
+				m=(sn[i].yp*70.0+y2+oy-(sn[i].yq*70.0+y2+oy))/(sn[i].xp*70.0+x0+30-(sn[i].xq*70.0+x0+30));
+			c=(sn[i].yp*70.0+y2+oy-(m*(sn[i].xp*70.0+x0+30)));
+			this->try2(i);
+			if((sn[i].yq-sn[i].yp)%2)
+			{
+				flag=!flag;
+				add*=(-1);
+			}
+		}
+	}
+	mt=1;
 }
 
 GLfloat kingf(GLfloat x,GLfloat m,GLfloat k)
@@ -369,11 +468,9 @@ void ladders()
 	GLfloat m;
 	glPointSize(20.0);
 	glColor3f(0.48,0.29,0);
-
 	switch(sel)
 	{
-		case 0:
-                ltorladder(1,0,4,2);
+		case 0:ltorladder(1,0,4,2);
                 l[0].xp=1,l[0].xq=4,l[0].yp=0,l[0].yq=2;
                 ltorladder(2,6,3,8);
                 l[1].xp=2,l[1].xq=3,l[1].yp=6,l[1].yq=8;
@@ -381,9 +478,8 @@ void ladders()
                 l[2].xp=8,l[2].xq=6,l[2].yp=4,l[2].yq=6;
                 rtolladder(5,4,3,5);
                 l[3].xp=5,l[3].xq=3,l[3].yp=4,l[3].yq=5;
-                break;
-		case 1:
-                ltorladder(1,1,4,4);
+                break;            
+		case 1: ltorladder(1,1,4,4);
                 l[0].xp=1,l[0].xq=4,l[0].yp=1,l[0].yq=4;
                 ltorladder(7,7,9,8);
                 l[1].xp=7,l[1].xq=9,l[1].yp=7,l[1].yq=8;
@@ -392,8 +488,7 @@ void ladders()
                 rtolladder(3,6,2,8);
                 l[3].xp=3,l[3].xq=2,l[3].yp=6,l[3].yq=8;
                 break;
-		case 2:
-                ltorladder(1,1,4,4);
+		case 2: ltorladder(1,1,4,4);
                 l[0].xp=1,l[0].xq=4,l[0].yp=1,l[0].yq=4;
                 ltorladder(2,6,3,8);
                 l[1].xp=2,l[1].xq=3,l[1].yp=6,l[1].yq=8;
@@ -403,25 +498,23 @@ void ladders()
                 l[3].xp=8,l[3].xq=7,l[3].yp=0,l[3].yq=2;
                 break;
 	}
-    // sn[0].xp=2,sn[0].yp=9,sn[0].xq=0,sn[0].yq=1;
-	// sn[1].xp=5,sn[1].yp=3,sn[1].xq=6,sn[1].yq=0;
-	// sn[2].xp=6,sn[2].yp=8,sn[2].xq=6,sn[2].yq=4;
-	// sn[3].xp=8,sn[3].yp=6,sn[3].xq=9,sn[3].yq=2;
+    sn[0].xp=2,sn[0].yp=9,sn[0].xq=0,sn[0].yq=1;
+	sn[1].xp=5,sn[1].yp=3,sn[1].xq=6,sn[1].yq=0;
+	sn[2].xp=6,sn[2].yp=8,sn[2].xq=6,sn[2].yq=4;
+	sn[3].xp=8,sn[3].yp=6,sn[3].xq=9,sn[3].yq=2;
 }
 
 void display2()
 {
     int i=0;
-    paw1.disp();
-	paw2.disp();
     glClearColor(0,0.50,0.50,1);
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
     glPushMatrix();
         glTranslatef(1200,600,0.0);
-		glRotatef(theta[0],1.0,1.0,0.0);
-        glRotatef(theta[1],0.0,1.0,0.0);
-   		glRotatef(theta[2],0.0,0.0,1.0);
+		glRotatef(theta[0],1.0,1.0,0.0);			//x-y
+        glRotatef(theta[1],0.0,1.0,0.0);			//y
+   		glRotatef(theta[2],0.0,0.0,1.0);			//z
         glTranslatef(-1200,-600,0.0);
         colorcube();
     glPopMatrix();
@@ -477,10 +570,119 @@ void display2()
 
     ladders();
     snakes();
+	paw1.disp();			//pawn1 at start
+	paw2.disp();			//pawn2 at start
+
+	
     glFlush();
     glutSwapBuffers();
 }
 
+void IR2(int a)			//update y
+{
+	if(alt==0)
+		paw1.updatey();
+	else
+		paw2.updatey();
+}
+void pawns::updatey()
+{
+	if(tmp&&t)
+	{
+		tmp-=speed;
+		q1+=speed;q2+=speed;q3+=speed;q4+=speed;
+		glutPostRedisplay();
+		glutTimerFunc(1,IR2,0);			//delay
+	}
+	else if(this->t&&tmp<=0)
+	{
+		t--;
+		add=add*(-1);
+		tmp=70;
+		this->init(t);
+		this->distance(t);
+	}
+	up=0;
+}
+
+void IR(int)			//decides which to move and calls distance()
+{
+	if(alt==0)
+		paw1.distance(r);
+	else
+		paw2.distance(r);
+}
+void pawns::distance(int r)			//move pawn
+{
+	if(flag)
+	{
+		if((p1-1)<380&&up)			//reached end of current row
+			this->updatey();
+		else if((p1+1)<=(1010)&&(this->t)&&tmp)
+		{
+			tmp-=speed;			//exact positions
+			p1+=add;p2+=add;p3+=add;p4+=add;
+			//glutPostRedisplay();
+			glutTimerFunc(1,IR,0);
+		}
+		else if(this->t&&tmp<=0)
+		{
+			t-=1;
+			tmp=70;
+			glutPostRedisplay();
+			glutTimerFunc(1,IR,0);
+		}
+		else  if((p1+1)>(1010))			//move up
+		{
+			up=1;
+			flag=0;
+			if(this->t)
+			{
+				tmp=70;
+				this->updatey();
+			}
+			if(this->t==0)			//ladders and snakes squares
+				this->check();
+		}
+		else if(this->t==0)			
+			this->check();
+	}
+	else
+	{
+		if(p1+1>1010&&this->t&&up)
+		{
+			this->updatey();
+		}
+		else if((p1-1)>=(380)&&this->t&&tmp)
+		{
+			tmp-=speed;
+			p1+=add,p2+=add,p3+=add,p4+=add;
+			glutPostRedisplay();
+			glutTimerFunc(1,IR,1);
+		}
+		else if(this->t&&tmp<=0)
+		{
+			t--;
+			tmp=70;
+			glutPostRedisplay();
+			glutTimerFunc(1,IR,1);
+		}
+		else if((p1-1)<(380))
+		{
+			up=1;
+			flag=1;
+			if(this->t)
+			{
+				tmp=70;
+				this->updatey();
+			}
+			if(this->t==0)
+				this->check();
+		}
+		else  if(this->t==0)
+			this->check();
+    }
+}
 int spincube()
 {
 	theta[0]+=12;
@@ -490,7 +692,7 @@ int spincube()
         theta[0]-=360;
 	}
     display2();
-	if(cnt<2)
+	if(cnt<2)			//no consecutive same number more than twice
         spincube();
     else
     {
@@ -522,17 +724,66 @@ int spincube()
 	return 0;
 }
 
+void display3()
+{
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.0,1.0,1.0,1.0);
+    int i;
+    glColor3f(1.0,0.0,0.0);
+    glRasterPos3f(600,380,10);
+	if(alt==0)
+        drawStringAtPos(500,500,10,"PLAYER 1 WINS");
+    else
+		drawStringAtPos(500,500,10,"PLAYER 2 WINS");
+    glutSwapBuffers();
+}
+
+void ir4(int a)
+{
+	glutDisplayFunc(display3);
+	glClearColor(0.0,1.0,1.0,1.0);
+    glutPostRedisplay();
+}
+
+int pawns::check100()
+{
+	int a=(p1-350)/70;			//row
+	int b=(q1-30)/70;			//col
+	if(b==9 && (a-r)<=0)
+	{
+		if(a-r<0)
+		{
+            mt=1;
+            return -1;
+		}
+		 if((a-r)==0)
+		{
+			this->init(r);
+			this->distance(r);
+			f=2;
+            glutTimerFunc((r+2)*1000,ir4,0);
+		}
+		return 1;
+	}
+	return 0;
+}
+
 void mymouse(int btn,int state,int x,int y)
 {
     if(!f)
     {
-            if(btn==GLUT_LEFT_BUTTON && state==GLUT_DOWN)
-            {
-               glutDisplayFunc(display2);
-               glutPostRedisplay();
-                f=1;
-            }
+		if(btn==GLUT_LEFT_BUTTON && state==GLUT_DOWN)
+		{
+			glutDisplayFunc(display2);
+			//glutPostRedisplay();
+			f=1;
+		}
     }
+	else if(f==2)
+	{
+		mt=0;
+		glutPostRedisplay();
+	}
     else
 	{
 		if(x>=((1140*width)/1367) && x<=((1260*width)/1367) && y<=(((767-260)*height)/767) && y>=(((767-340)*height)/767))
@@ -545,6 +796,23 @@ void mymouse(int btn,int state,int x,int y)
                 theta[2]=45;
 				mt=0;
 				int a=spincube();
+				alt=!alt;			//switch between players
+				if(alt==0)
+				{
+					if(paw1.check100()==0)
+					{
+						paw1.init(r);
+						paw1.distance(r);
+					}
+				}
+				else
+				{
+					if(paw2.check100()==0)
+					{
+						paw2.init(r);
+						paw2.distance(r);
+					}
+				}
 			}
 		}
 	}
@@ -577,7 +845,7 @@ int main(int argc,char *argv[])
     srand(time(NULL));
 	sel=rand()%3;
     glEnable(GL_POLYGON_SMOOTH);
-    //init();
+    //myinit();
     glutDisplayFunc(display1);
     glEnable(GL_DEPTH_TEST);
     glutMouseFunc(mymouse);
@@ -585,40 +853,3 @@ int main(int argc,char *argv[])
     glutMainLoop();
     return 0;
 }
-
-//snvl0.h
-#ifndef snvl0
-#define snvl0
-#include<GL/glut.h>
-#define maxx 10			//no of rows in board
-#define maxy 10			//no of cols in board
-#define dx 70			//x dist of each sqaure in board
-#define dy 70			//y dist of each square in board
-#define x0 350			//Begin x of board
-#define y2 30			//Begin y of board
-#define speed 5			//speed of movement of pawns
-GLfloat x[maxx+1]={0},y[maxy+1]={0};
-//GLfloat p1=x0-40,p2=x0-50,p3=x0-40,p4=x0-30,q1=y2+30,q2=y2+40,q3=y2+50,q4=y2+40;
-
-extern void rect();
-extern void Cylinder_draw();
-extern void cyl();
-class pawns
-{
-	private:
-		float p1,p2,p3,p4,q1,q2,q3,q4,oy,m,c,col1;
-		int add,up,tmp,t,flag;
-	public:
-		pawns(GLfloat,GLfloat,GLfloat,GLint tmp);
-		void distance(int);
-		void updatey();
-		void disp();
-		void init(int r);
-		void check();
-		int check100();
-		void tryy(int i);
-		void try2(int i);
-		
-		
-};
-#endif
